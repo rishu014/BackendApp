791b83efb30873e12ac34d4ef092c9ab8d86b4a1
rishu014 <rishabhgusain622@gmail.com>
2022-11-25 21:31:52 +0530

    Deploying Backend

---
+++ src/main/java/com/nagarro/lms/model/BookResponseError.java
@@ -0,0 +1,38 @@
+package com.nagarro.lms.model;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author rishabhgusain
+ *
+ */
+@Component
+public class BookResponseError {
+    private int status;
+    private String message;
+    private long timestamp;
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+}
